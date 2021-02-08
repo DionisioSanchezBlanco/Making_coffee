b3c3b05966c13e8145f6307db332e0e2894d33b1
Dioni00 <dsanbla@gmail.com>
2021-02-08 17:00:57 +0100

    Second stage

--- coffee.py
+++ coffee.py
@@ -1,9 +1,12 @@
 # Making coffee
 
-print("""Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!""")
+def quantities(coffees):
+    print(f"{coffees * 200} ml of water")
+    print(f"{coffees * 50} ml of milk")
+    print(f"{coffees * 15} g of coffee beans")
+
+print("Write how many cups of coffee you will need:")
+coffees = int(input())
+
+print(f"For {coffees} cups of coffee you will need:")
+quantities(coffees)
