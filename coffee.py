bac7aef543eea9afd139188e81365a1fbaa6e3d0
Dioni00 <dsanbla@gmail.com>
2021-02-09 18:39:53 +0100

    Stage 3

--- coffee.py
+++ coffee.py
@@ -1,12 +1,28 @@
 # Making coffee
+water, milk, beans = 200, 50, 15
 
-def quantities(coffees):
-    print(f"{coffees * 200} ml of water")
-    print(f"{coffees * 50} ml of milk")
-    print(f"{coffees * 15} g of coffee beans")
+def answer(coffees, w, m, b):
+    water_coffees = w // water
+    milk_coffees = m // milk
+    beans_coffees = b // beans
+
+    if coffees == min(water_coffees, milk_coffees, beans_coffees):
+        print("Yes, I can make that amount of coffee")
+    elif coffees > min(water_coffees, milk_coffees, beans_coffees):
+        print(f"No, I can make only {min(water_coffees, milk_coffees, beans_coffees)} cups of coffee")
+    else:
+        print(f"Yes, I can make that amount of coffee (and even {min(water_coffees, milk_coffees, beans_coffees) - coffees} more than that)")
+
+print("Write how many ml of water the coffee machine has:")
+w = int(input())
+
+print("Write how many ml of milk the coffee machine has:")
+m = int(input())
+
+print("Write how many grams of coffee beans the coffee machine has:")
+b = int(input())
 
 print("Write how many cups of coffee you will need:")
 coffees = int(input())
 
-print(f"For {coffees} cups of coffee you will need:")
-quantities(coffees)
+answer(coffees, w, m, b)
